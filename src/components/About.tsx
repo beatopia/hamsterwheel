1083e7901a477a5c55df2b0d10f56164447a25f3
beatopia <kailuzniak@gmail.com>
2026-05-07 22:50:37 -0700

    changed font to be more readable

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -29,47 +29,49 @@ export default function About({ setPage }: PageProps) {
   return (
     <section className="about-page" aria-labelledby="about-title">
       <p className="about-kicker">About</p>
-      <h1 id="about-title" className="about-title">
+      <h1 id="about-title" className="about-title about-section about-inter">
         Hey! I&apos;m Kai Luzniak, a curious computer science student.
       </h1>
 
-      <div className="about-group">
-        <div className="about-line">
-          <p className="about-label">Currently building</p>
-          <div className="about-chip about-chip--featured" aria-label="Slug Gaming">
-            <img
-              src={currentProject.logo}
-              alt="Slug Gaming logo"
-              className="about-chip-logo"
-            />
-            <span>{currentProject.name}</span>
+      <div className="about-section about-works">
+        <div className="about-group">
+          <div className="about-line">
+            <p className="about-label about-inter">Currently building</p>
+            <div className="about-chip about-chip--featured" aria-label="Slug Gaming">
+              <img
+                src={currentProject.logo}
+                alt="Slug Gaming logo"
+                className="about-chip-logo"
+              />
+              <span className="about-inter">{currentProject.name}</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="about-group about-group--interned">
+          <div className="about-line">
+            <p className="about-label about-inter">Previously interned at</p>
+            {previousInternships.map((company) => (
+              <div key={company.name} className="about-chip" aria-label={company.name}>
+                <img
+                  src={company.logo}
+                  alt={`${company.name} logo`}
+                  className="about-chip-logo"
+                />
+                <span className="about-inter">{company.name}</span>
+              </div>
+            ))}
           </div>
         </div>
       </div>
 
-      <div className="about-group about-group--interned">
-        <div className="about-line">
-          <p className="about-label">Previously interned at</p>
-          {previousInternships.map((company) => (
-            <div key={company.name} className="about-chip" aria-label={company.name}>
-              <img
-                src={company.logo}
-                alt={`${company.name} logo`}
-                className="about-chip-logo"
-              />
-              <span>{company.name}</span>
-            </div>
-          ))}
-        </div>
-      </div>
-
-      <p className="about-copy">
+      <p className="about-copy about-section about-copy--spaced about-inter">
         I am enthusiastic about building tools that help myself and others to be more
         productive and enjoy the process of crafting. You can find my{' '}
         <Link to="/projects">full projects list here</Link>.
       </p>
 
-      <p className="about-copy">
+      <p className="about-copy about-copy--spaced about-inter">
         At UC Santa Cruz, I support the 3,000+ members of Slug Gaming as one of four
         executive signers, help manage the UCSC Computer Lounges as a systems
         administrator, represent Slug Gaming through Gen.G Collegiate, and intern with
@@ -77,7 +79,7 @@ export default function About({ setPage }: PageProps) {
         player.
       </p>
 
-      <p className="about-copy">
+      <p className="about-copy about-inter">
         Outside of programming, I love exploring nature, climbing, cooking, and writing
         about music. I&apos;m pretty enamored with my favorite songs, so check out my{' '}
         <Link to="/blog">blog</Link> if you get a chance! :3
