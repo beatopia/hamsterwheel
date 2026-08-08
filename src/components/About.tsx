0453acfb70f50529192b2944dc5be72d0c74be8b
beatopia <kailuzniak@gmail.com>
2026-08-07 20:22:07 -0700

    fixed about me

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -97,7 +97,7 @@ export default function About({ setPage }: PageProps) {
     <section className="about-page" aria-labelledby="about-title">
       <p className="about-kicker">About</p>
       <h1 id="about-title" className="about-title about-inter">
-        Hey! I&apos;m Kai Luzniak, a curious CS student who is always looking for ways to learn and improve.
+        Hey! I&apos;m Kai Luzniak, a curious CS student who is always looking for ways to improve.
       </h1>
 
       <div className="about-section about-works">
